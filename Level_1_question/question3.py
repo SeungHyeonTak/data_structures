52eb2736302b7617c19929cfa203c2b035209d2e
SeungHyeonTak <conficker77@gmail.com>
2019-09-14 02:23:29 +0900

    git lf test_2

--- Level_1_question/question3.py
+++ Level_1_question/question3.py
@@ -1,1 +1,1 @@
-# 완주하지 못한 선수class Node:    def __init__(self):        self.data = None        self.next = None# git ^M에 의한 test commitclass text2:    def __init__(self):        self.test = None
+# 완주하지 못한 선수class Node:    def __init__(self):        self.data = None        self.next = None# git ^M에 의한 test commitclass text2:    def __init__(self):        self.test = None# test222class text2:    def __init__(self):        self.test = None
