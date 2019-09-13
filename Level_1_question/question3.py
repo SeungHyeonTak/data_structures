0113b5c4fbd5884eeed4ca2b2901920c5f2dd95f
SeungHyeonTak <conficker77@gmail.com>
2019-09-14 02:19:30 +0900

    버그로 인해 all_update_0914

---
+++ Level_1_question/question3.py
@@ -0,0 +1,1 @@
+# 완주하지 못한 선수class Node:    def __init__(self):        self.data = None        self.next = None# git ^M에 의한 test commitclass text2:    def __init__(self):        self.test = None
