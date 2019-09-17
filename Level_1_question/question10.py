52cdd56fef6132705f2ee40ebe987bf87749e70f
SeungHyeonTak <conficker77@gmail.com>
2019-09-17 18:41:17 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Level_1_question/question10.py
@@ -0,0 +1,1 @@
+# x만큼 간격이 있는 n개의 숫자def solution(x, n):    answer = []    for i in range(1, n + 1):        answer.append(i * x)    return answerif __name__ == "__main__":    x = 4    n = 3    print(solution(x, n))
