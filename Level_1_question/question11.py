85d086a4dae234e17e4a3f89a4f5376621ec30d0
SeungHyeonTak <conficker77@gmail.com>
2019-09-30 17:43:59 +0900

    같은 숫자는 싫어

---
+++ Level_1_question/question11.py
@@ -0,0 +1,1 @@
+# 같은 숫자는 싫어def solution(arr):    answer = []    for i in range(len(arr)):        if i == 0:            answer.append(arr[i])        elif arr[i] != arr[i - 1]:            answer.append(arr[i])    return answerif __name__ == "__main__":    arr = [4, 4, 4, 3, 3]    print(solution(arr))
